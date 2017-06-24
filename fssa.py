c607c04a3d1d53fcaa6c1a96e207e675147c30f4
Alexander Lee <cloudstrife79@gmail.com>
2017-06-24 01:26:45 -0400

    link and speaker data; nostem initial dev

--- fssa.py
+++ fssa.py
@@ -7,6 +7,7 @@ from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import pandas as pd
+from textblob import TextBlob
 
 #speechurl = 'https://www.federalreserve.gov/feeds/speeches.xml'
 #pre2010 = 'https://www.federalreserve.gov/newsevents/speech/2010speech.htm'
@@ -34,8 +35,9 @@ for date in pre2010dates:
     pre2010content[date] = page.content
     print('fetched content for {}'.format(date))
 
-# get content links for pre-2010 speech data
+# get content links and speakers for pre-2010 speech data
 pre2010links = {}
+pre2010speakers = {}
 for date in pre2010content.keys():
     soup = BeautifulSoup(pre2010content[date], 'lxml')
     # ul id='speechIndex' is what we want
@@ -46,15 +48,22 @@ for date in pre2010content.keys():
     for li in speechIndices:
         # get the speech date
         tagtext = li.get_text()
-        speechdate = re.search('[A-Z][a-z]+ \d+, \d+', tagtext)
+        speechdate = re.search('[A-Z][a-z]+ \d{1,2}, \d{4}', tagtext)
         # get the link to the speech
         postfix = li.a.attrs['href']
         pre2010links[speechdate.group(0)] = urlstem + postfix
-        print('Recorded url for speech "{}"'.format(li.a.get_text()))
-    print('Finished parsing links for {}'.format(date))
+        # get the speaker info
+        speaker = li.find('div', attrs = {'class': 'speaker'}).get_text()
+        speaker = re.sub('\s{2,}', '', speaker)
+        # fix for weird error I'm seeing:
+        speaker = re.sub('\xa0', ' ', speaker)
+        pre2010speakers[speechdate.group(0)] = speaker
+        print('Recorded url and speaker for speech "{}"'.format(li.a.get_text()))
+    print('Finished parsing links and speakers for {}'.format(date))
 
 # parse content links for pre-2010 speech data
 pre2010speeches = {}
+NOSTEM_pre2010 = {}
 for k in pre2010links.keys():
     page = requests.get(pre2010links[k])
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -85,6 +94,7 @@ for k in pre2010links.keys():
                 clean_sent.append(word)
         clean_sent = ' '.join(clean_sent)
         cleaned[i] = clean_sent
+    NOSTEM_pre2010[k] = cleaned.copy()
     # stem words
     cleaned = [' '.join([stemmer.stem(word) for word in sent.split()]) for sent in cleaned]
     # index list of cleaned speech text tokens into dict
@@ -103,6 +113,8 @@ pre2010data['Datestamp'] = [pd.Timestamp(date) for date in pre2010data['Date']]
 pre2010data.sort_values('Datestamp', inplace = True)
 pre2010data.index = range(len(pre2010data))
 
+# TODO: NOSTEM version of this file
+
 # post-2010 data
 post2010content = {}
 for date in post2010dates:
@@ -112,8 +124,9 @@ for date in post2010dates:
     post2010content[date] = page.content
     print('fetched content for {}'.format(date))
 
-# get content links for post-2010 speech data
+# get content links and speakers for post-2010 speech data
 post2010links = {}
+post2010speakers = {}
 for date in post2010content.keys():
     soup = BeautifulSoup(post2010content[date], 'html.parser')
     # need to find tag for date info and link to speech content
@@ -128,14 +141,19 @@ for date in post2010content.keys():
         timelist.append(time.time.get_text())
     for event in events:
         eventlinks.append(event.a.get('href'))
-    assert len(timelist) == len(eventlinks)
+    speakers = []
+    for event in events:
+        speakers.append(event.find('p', attrs = {'class': 'news__speaker'}).get_text())
+    assert len(timelist) == len(eventlinks) == len(speakers)
     for i in range(len(timelist)):
         post2010links[timelist[i]] = urlstem + eventlinks[i]
-        print('Recorded url for speech "{}"'.format(events[i].a.get_text()))
+        post2010speakers[timelist[i]] = speakers[i]
+        print('Recorded url and speaker for speech "{}"'.format(events[i].a.get_text()))
     print('Finished parsing links for {}'.format(date))
 
 # parse content links for post-2010 speech data
 post2010speeches = {}
+NOSTEM_post2010 = {}
 for k in post2010links.keys():
     page = requests.get(post2010links[k])
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -160,6 +178,10 @@ for k in post2010links.keys():
                 clean_sent.append(word)
         clean_sent = ' '.join(clean_sent)
         cleaned[i] = clean_sent
+    NOSTEM_post2010[k] = cleaned.copy()
+
+    # TODO: reorder some of this cleaning to eliminate problem fragments in nostem version
+
     # stem words
     cleaned = [' '.join([stemmer.stem(word) for word in sent.split()]) for sent in cleaned]
     # elimintate a couple other things
@@ -186,13 +208,49 @@ post2010data['Datestamp'] = [pd.Timestamp(date) for date in post2010data['Date']
 post2010data.sort_values('Datestamp', inplace = True)
 post2010data.index = range(len(pre2010data), len(pre2010data) + len(post2010data))
 
+# TODO: NOSTEM version of this file
+
 # combine data
 speechdata = pre2010data.append(post2010data)
+# TODO: NOSTEM version of this file
+
+# get links dataframe
+links = pd.DataFrame()
+i = 0
+for day in pre2010links.keys():
+    links.loc[i, 'Date'] = str(pd.Timestamp(day).date())
+    links.loc[i, 'URL'] = pre2010links[day]
+    i += 1
+for day in post2010links.keys():
+    links.loc[i, 'Date'] = str(pd.Timestamp(day).date())
+    links.loc[i, 'URL'] = post2010links[day]
+    i += 1
+links.sort_values('Date', inplace = True)
+links.index = range(len(links))
+links.to_csv('C:/Users/Alex/Dropbox/Projects/fssa/links.csv')
+
+# get speakers dataframe
+speakers = pd.DataFrame()
+i = 0
+for day in pre2010speakers.keys():
+    speakers.loc[i, 'Date'] = str(pd.Timestamp(day).date())
+    speakers.loc[i, 'Speaker'] = pre2010speakers[day]
+    i += 1
+for day in post2010speakers.keys():
+    speakers.loc[i, 'Date'] = str(pd.Timestamp(day).date())
+    speakers.loc[i, 'Speaker'] = post2010speakers[day]
+    i += 1
+speakers.sort_values('Date', inplace = True)
+speakers.index = range(len(speakers))
+speakers.to_csv('C:/Users/Alex/Dropbox/Projects/fssa/speakers.csv')
 
 # get compound sentiment
 vaderanalyzer = SentimentIntensityAnalyzer()
 speechdata['Sentiment'] = [vaderanalyzer.polarity_scores(speechdata['Token'][i])['compound'] \
                             for i in range(len(speechdata))]
+
+# compare with textblob sentiment
+speechdata['Sentiment_TB'] = [TextBlob(i).sentiment.polarity for i in speechdata['Token']]
 
 speechdata.to_csv('C:/Users/Alex/Dropbox/Projects/fssa/speechdata.csv')
 
@@ -226,6 +284,8 @@ plt.show()
 # nothing immediately clear but we could do something like better transforms
 # (e.g. GARCH for S&P prices), VAR Granger test, etc. and see if we find anything more interesting
 
+
+# TODO: compare TextBlob sentiment vs. vader
 
 # NOTES
 
